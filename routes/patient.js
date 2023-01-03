bfbff9b916244af0f6e509d3604c27cc0daf9a05
Mohamad-Aj <braaokbe42@gmail.com>
2023-01-03 11:35:57 +0200

    added Doctors/Nurses List,Booking appointment and delete it also and guid and update profile also added a deleteLab feature

--- routes/patient.js
+++ routes/patient.js
@@ -30,12 +30,6 @@ router.route('/register').get((req, res) => {
     res.render('patient/register')
 })
 
-
-router.get('/:id', (req, res) => {
-    const id = req.params.id;
-    res.render('patient/Index', { id: id })
-})
-
 router.get('/:id', (req, res) => {
     if (session) {
         const id = req.params.id;
@@ -63,6 +57,26 @@ router.get('/Index/:id', (req, res) => {
     }
 })
 
+router.get('/Appointments/:id', (req, res) => {
+    if (session) {
+        const DocRef = db.collection('alldoctors');
+        const appRef = db.collection('allappoints');
+        DocRef.find().toArray((err, arr) => {
+            const id = req.params.id;
+            User.findById(id)
+                .then(result => {
+                    appRef.find().toArray((err, a) => {
+                        if (result) res.render('patient/Appointments', { apps: a, doctors: arr, result: result })
+                    })
+
+                })
+        })
+    } else {
+        res.redirect('/HomePage')
+    }
+
+})
+
 router.get('/Medicine/:id', (req, res) => {
     if (session) {
         const id = req.params.id;
@@ -76,12 +90,39 @@ router.get('/Medicine/:id', (req, res) => {
     }
 })
 
+
+router.get('/Profile/:id', (req, res) => {
+    if (session) {
+        const id = req.params.id;
+        User.findById(id)
+            .then(result => {
+                if (result) res.render('patient/Profile', { result: result })
+            })
+    }
+    else {
+        res.redirect('/HomePage')
+    }
+})
+
 router.get('/OrderCard/:id', (req, res) => {
     if (session) {
         const id = req.params.id;
         User.findById(id)
             .then(result => {
                 if (result) res.render('patient/OrderCard', { result: result })
+            })
+    }
+    else {
+        res.redirect('/HomePage')
+    }
+})
+
+router.get('/Doctors/:id', (req, res) => {
+    if (session) {
+        const id = req.params.id;
+        User.findById(id)
+            .then(result => {
+                if (result) res.render('patient/Doctors', { result: result })
             })
     }
     else {
@@ -107,6 +148,18 @@ router.get('/Rate/:id', (req, res) => {
             })
     }
     else {
+        res.redirect('/HomePage')
+    }
+})
+
+router.get('/Guide/:id', (req, res) => {
+    if (session) {
+        const id = req.params.id;
+        User.findById(id)
+            .then(result => {
+                if (result) res.render('patient/Guide', { result: result })
+            })
+    } else {
         res.redirect('/HomePage')
     }
 })
@@ -167,6 +220,283 @@ router.route('/login').post(async (req, res) => {
     }
 })
 
+
+router.route('/Appointments/:id/:patname').post(async (req, res) => {
+    const DocName = req.body.docs;
+    const date = req.body.date.toString();
+    const time = req.body.time.toString();
+    var New_Appointment1 = {
+        time: time,
+        date: date,
+        name: DocName
+    }
+    let patN;
+    console.log(DocName)
+    const id = req.params.id;
+    let flag2 = 0
+    User.findById(id)
+        .then(async r => {
+            console.log(r)
+            var Patient = [r.fullname, ~~((Date.now() - r.birthdate) / (31557600000)), r.ID];
+            let flag = 0;
+            const appRef = db.collection('allappoints');
+            await appRef.find().toArray(async (err, a) => {
+                console.log(a)
+                if (a.length >= 0) {
+                    if (a.length > 0) {
+                        await a.forEach(appos => {
+                            console.log(appos)
+                            console.log(appos.time, New_Appointment1.time)
+                            console.log(appos.date, New_Appointment1.date)
+                            if (appos.time === New_Appointment1.time && appos.date === New_Appointment1.date && appos.name === DocName) {
+                                console.log('in if statement')
+                                flag2 = 1
+                            }
+                        })
+                    }
+                    if (a.length >= 0) {
+                        r.appointments.forEach(appo1=>{
+                            if(appo1.time === New_Appointment1.time && appo1.date === New_Appointment1.date){
+                                flag2 = 2;
+                            }
+                        })
+                        if (flag2 === 0) {
+                            console.log('the flag2 didnt change')
+                            db.collection('users').updateOne({ fullname: r.fullname }, {
+                                $push: {
+                                    appointments: New_Appointment1
+                                }
+                            })
+                            db.collection('allappoints').insertOne(New_Appointment1)
+
+                        }
+                    }
+                }
+
+            })
+
+            console.log('flag2=', flag2)
+            const date1 = req.body.date.toString();
+            const time1 = req.body.time.toString();
+            var New_Appointment = {
+                name: req.params.patname,
+                time: time1,
+                date: date1,
+            }
+            let flag1 = 0
+            console.log(DocName)
+            User.findOne({ fullname: DocName })
+                .then((user) => {
+                    if (user) {
+                        // console.log(user)
+                        Doctor.findOne({ IDS: user.ID })
+                            .then(async (result) => {
+                                if (result) {
+                                    let fla = 0
+                                    // console.log(New_Appointment)
+                                    // console.log(user)
+                                    // console.log(result)
+                                    // console.log(user.fullname)
+                                    // console.log(result)
+                                    // console.log(result.patients)
+                                    if (user.patients.length > 0) {
+                                        user.patients.forEach(patient => {
+                                            // console.log('im hereeee')
+                                            // console.log(patient)
+                                            if (patient[0] === Patient[0] && patient[1] === Patient[1] && patient[2] === Patient[2]) {
+                                                fla = 1;
+                                            }
+                                        })
+                                    }
+                                    if (fla === 0) {
+                                        db.collection('users').updateOne({ fullname: DocName }, {
+                                            $push: {
+                                                patients: Patient
+                                            }
+                                        })
+                                    }
+
+                                    user.appointments.forEach(appo => {
+                                        if (appo.time === New_Appointment.time && appo.date === New_Appointment.date)
+                                            flag1 = 1
+                                    })
+                                    if (flag1 === 0 && flag2===0) {
+                                        db.collection('users').updateOne({ fullname: user.fullname }, {
+                                            $push: {
+                                                appointments: New_Appointment
+                                            }
+                                        })
+                                    }
+                                }
+
+                            });
+                            db.collection('nurses').findOne({ PDS: user.ID })
+                            .then(async (result3) => {
+                                if (result3) {
+                                    let fla = 0
+                                    // console.log(New_Appointment)
+                                    // console.log(user)
+                                    // console.log(result)
+                                    // console.log(user.fullname)
+                                    // console.log(result)
+                                    // console.log(result.patients)
+                                    if (user.patients.length > 0) {
+                                        user.patients.forEach(patient => {
+                                            // console.log('im hereeee')
+                                            // console.log(patient)
+                                            if (patient[0] === Patient[0] && patient[1] === Patient[1] && patient[2] === Patient[2]) {
+                                                fla = 1;
+                                            }
+                                        })
+                                    }
+                                    if (fla === 0) {
+                                        db.collection('users').updateOne({ fullname: DocName }, {
+                                            $push: {
+                                                patients: Patient
+                                            }
+                                        })
+                                    }
+
+                                    user.appointments.forEach(appo => {
+                                        if (appo.time === New_Appointment.time && appo.date === New_Appointment.date)
+                                            flag1 = 1
+                                    })
+                                    if (flag1 === 0) {
+                                        db.collection('users').updateOne({ fullname: user.fullname }, {
+                                            $push: {
+                                                appointments: New_Appointment
+                                            }
+                                        })
+                                    }
+                                }
+
+                            });
+                        res.redirect(req.get('referer'));
+                    }
+                });
+        });
+
+    // console.log(DocName)
+
+})
+
+router.route('/deleteAppoint/:id/:time/:date/:docname').post(async (req, res) => {
+    const time = req.params.time;
+    const id = req.params.id;
+    const date = req.params.date;
+    const docname = req.params.docname;
+    console.log(id);
+    User.findById(id)
+        .then(async (result) => {
+            if (result) {
+                console.log(time)
+                await db.collection('users').updateOne({ fullname: result.fullname }, { $pull: { "appointments": { name: docname, date: date, time: time } } })
+                db.collection('users').updateOne({ fullname: docname }, { $pull: { "appointments": { name: result.fullname, date: date, time: time } } })
+                db.collection('allappoints').deleteOne({ name: docname, time: time, date: date })
+                res.redirect(req.get('referer'));
+
+            }
+        })
+})
+
+router.route('/Profile/:id').put(async (req, res) => {
+    console.log('im here here')
+    const email = req.body.email.toLowerCase();
+    console.log(email)
+
+    const phonenumber = req.body.phonenumber;
+    var hashedPassword = await bcrypt.hash(req.body.password, 10);
+    console.log(req.body.password)
+    console.log(phonenumber)
+    const id = req.params.id;
+    User.findById(id)
+        .then(result => {
+            if (result) {
+                if (email && phonenumber && req.body.password) {
+                    db.collection('users').updateOne({ email: result.email }, {
+                        $set: {
+                            email: email,
+                            phonenumber: phonenumber,
+                            password: hashedPassword
+                        }
+
+                    })
+                    res.redirect(req.get('referer'));
+                }
+                else {
+                    if (email && phonenumber && req.body.password.length === 0) {
+                        db.collection('users').updateOne({ email: result.email }, {
+                            $set: {
+                                email: email,
+                                phonenumber: phonenumber
+                            }
+
+                        })
+                        res.redirect(req.get('referer'));
+                    }
+                    else {
+                        if (email && req.body.password && phonenumber.length === 0) {
+                            db.collection('users').updateOne({ email: result.email }, {
+                                $set: {
+                                    email: email,
+                                    password: hashedPassword
+                                }
+
+                            })
+                            res.redirect(req.get('referer'));
+                        }
+                        else {
+                            if (email.length === 0 && phonenumber && req.body.password) {
+                                db.collection('users').updateOne({ email: result.email }, {
+                                    $set: {
+                                        phonenumber: phonenumber,
+                                        password: hashedPassword
+                                    }
+
+                                })
+                                res.redirect(req.get('referer'));
+                            }
+                            else {
+                                if (email && phonenumber.length === 0 && req.body.password.length === 0) {
+                                    console.log('change only email')
+                                    db.collection('users').updateOne({ email: result.email }, {
+                                        $set: {
+                                            email: email
+                                        }
+
+                                    })
+                                    res.redirect(req.get('referer'));
+                                }
+                                else {
+                                    if (email.length === 0 && phonenumber && req.body.password.length === 0) {
+                                        db.collection('users').updateOne({ email: result.email }, {
+                                            $set: {
+                                                phonenumber: phonenumber
+                                            }
+
+                                        })
+                                        res.redirect(req.get('referer'));
+                                    }
+                                    else {
+                                        if (email.length === 0 && phonenumber.length === 0 && req.body.password) {
+                                            db.collection('users').updateOne({ email: result.email }, {
+                                                $set: {
+                                                    password: hashedPassword
+                                                }
+
+                                            })
+                                            res.redirect(req.get('referer'));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        })
+
+})
 
 router.route('/Rate/:id').post(async (req, res) => {
     console.log('im in rate')
@@ -263,6 +593,21 @@ router.route('/OrderCard/:id').post((req, res) => {
 
 })
 
+
+router.route('/deleteLab/:id/:Result/:nurse').post(async (req, res) => {
+    const id = req.params.id;
+    const Res = req.params.Result;
+    const Nurse = req.params.nurse
+    User.findById(id)
+    .then(result=>{
+        if(result){
+            db.collection('users').updateOne({fullname:result.fullname},{
+                $pull:{"notes": {Nurse:Nurse,Result:Res}}
+            })
+            res.redirect(req.get('referer'))
+        }
+    })
+})
 
 router.route('/LogOut').post((req, res) => {
     console.log('loggin out in patient')
